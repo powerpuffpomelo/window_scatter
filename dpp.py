666d455a21f064ed432245aa35b958a4753b6c3d
mello <powerpuffpomelo@gmail.com>
2022-06-05 20:33:56 +0800

    update

--- dpp.py
+++ dpp.py
@@ -1,22 +1,23 @@
 import numpy as np
 import math
+import time
 
 
-def dpp(kernel_matrix, max_length, epsilon=1E-10):
+def dpp(kernel_matrix, ret_length, epsilon=1E-10):
     """
     Our proposed fast implementation of the greedy algorithm
     :param kernel_matrix: 2-d array
-    :param max_length: positive int
+    :param ret_length: positive int
     :param epsilon: small positive scalar
     :return: list
     """
     item_size = kernel_matrix.shape[0]
-    cis = np.zeros((max_length, item_size))
+    cis = np.zeros((ret_length, item_size))
     di2s = np.copy(np.diag(kernel_matrix))
     selected_items = list()
     selected_item = np.argmax(di2s)
     selected_items.append(selected_item)
-    while len(selected_items) < max_length:
+    while len(selected_items) < ret_length:
         k = len(selected_items) - 1
         ci_optimal = cis[:k, selected_item]
         di_optimal = math.sqrt(di2s[selected_item])
@@ -31,53 +32,22 @@ def dpp(kernel_matrix, max_length, epsilon=1E-10):
         selected_items.append(selected_item)
     return selected_items
 
+def main():
+    item_size = 100    # 视频数量
+    feature_dimension = 64
+    ret_length = 8
 
-def dpp_sw(kernel_matrix, window_size, max_length, epsilon=1E-10):
-    """
-    Sliding window version of the greedy algorithm
-    :param kernel_matrix: 2-d array
-    :param window_size: positive int
-    :param max_length: positive int
-    :param epsilon: small positive scalar
-    :return: list
-    """
-    item_size = kernel_matrix.shape[0]
-    v = np.zeros((max_length, max_length))
-    cis = np.zeros((max_length, item_size))
-    di2s = np.copy(np.diag(kernel_matrix))
-    selected_items = list()
-    selected_item = np.argmax(di2s)
-    selected_items.append(selected_item)
-    window_left_index = 0
-    while len(selected_items) < max_length:
-        k = len(selected_items) - 1
-        ci_optimal = cis[window_left_index:k, selected_item]
-        di_optimal = math.sqrt(di2s[selected_item])
-        v[k, window_left_index:k] = ci_optimal
-        v[k, k] = di_optimal
-        elements = kernel_matrix[selected_item, :]
-        eis = (elements - np.dot(ci_optimal, cis[window_left_index:k, :])) / di_optimal
-        cis[k, :] = eis
-        di2s -= np.square(eis)
-        if len(selected_items) >= window_size:
-            window_left_index += 1
-            for ind in range(window_left_index, k + 1):
-                t = math.sqrt(v[ind, ind] ** 2 + v[ind, window_left_index - 1] ** 2)
-                c = t / v[ind, ind]
-                s = v[ind, window_left_index - 1] / v[ind, ind]
-                v[ind, ind] = t
-                v[ind + 1:k + 1, ind] += s * v[ind + 1:k + 1, window_left_index - 1]
-                v[ind + 1:k + 1, ind] /= c
-                v[ind + 1:k + 1, window_left_index - 1] *= c
-                v[ind + 1:k + 1, window_left_index - 1] -= s * v[ind + 1:k + 1, ind]
-                cis[ind, :] += s * cis[window_left_index - 1, :]
-                cis[ind, :] /= c
-                cis[window_left_index - 1, :] *= c
-                cis[window_left_index - 1, :] -= s * cis[ind, :]
-            di2s += np.square(cis[window_left_index - 1, :])
-        di2s[selected_item] = -np.inf
-        selected_item = np.argmax(di2s)
-        if di2s[selected_item] < epsilon:
-            break
-        selected_items.append(selected_item)
-    return selected_items
+    scores = np.exp(0.01 * np.random.randn(item_size) + 0.2)  # 相关性分数
+    feature_vectors = np.random.randn(item_size, feature_dimension)
+
+    feature_vectors /= np.linalg.norm(feature_vectors, axis=1, keepdims=True)  # 归一化
+    similarities = np.dot(feature_vectors, feature_vectors.T)
+    kernel_matrix = scores.reshape((item_size, 1)) * similarities * scores.reshape((1, item_size))
+
+    t = time.time()
+    result = dpp(kernel_matrix, ret_length)
+    print('algorithm running time: ' + '\t' + "{0:.4e}".format(time.time() - t))
+    print(result)
+
+if __name__ == "__main__":
+    main()
